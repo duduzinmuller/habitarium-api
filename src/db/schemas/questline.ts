e0bb6da53958c03b226867676b1b24b11569341b
duduzinmuller <eduardomuller989@gmail.com>
2025-09-23 11:11:46 -0400

    feat(questlines): introduce questline status enum and update questlines schema

--- src/db/schemas/questline.ts
+++ src/db/schemas/questline.ts
@@ -1,46 +1,23 @@
 import {
   integer,
-  json,
+  pgEnum,
   pgTable,
   text,
   timestamp,
   uuid,
 } from "drizzle-orm/pg-core";
-import { boolean } from "drizzle-orm/pg-core";
-import { characters } from "./characters";
+
+export const questlineStatusEnum = pgEnum("questline_status", [
+  "locked",
+  "unlocked",
+  "completed",
+]);
 
 export const questlines = pgTable("questlines", {
   id: uuid("id").defaultRandom().primaryKey(),
   title: text("title").notNull(),
   description: text("description"),
-  createdAt: timestamp("created_at").defaultNow().notNull(),
-});
-
-export const lessons = pgTable("lessons", {
-  id: uuid("id").defaultRandom().primaryKey(),
-  questlineId: uuid("questline_id")
-    .notNull()
-    .references(() => questlines.id, { onDelete: "cascade" }),
-  title: text("title").notNull(),
-  description: text("description"),
-  xp: integer("xp").notNull(),
-  difficulty: text("difficulty").notNull(),
-  icon: text("icon"),
-  content: json("content"),
-  sequenceIndex: integer("sequence_index").notNull(),
-  createdAt: timestamp("created_at").defaultNow().notNull(),
-});
-
-export const lessonsProgress = pgTable("lessons_progress", {
-  id: uuid("id").defaultRandom().primaryKey(),
-  characterId: uuid("character_id")
-    .notNull()
-    .references(() => characters.id, { onDelete: "cascade" }),
-  lessonId: uuid("lesson_id")
-    .notNull()
-    .references(() => lessons.id, { onDelete: "cascade" }),
-  completed: boolean("completed").notNull().default(false),
-  locked: boolean("locked").notNull().default(false),
-  progress: integer("progress").notNull().default(0),
+  sequenceIndex: integer("sequence_index").notNull().unique(),
+  status: questlineStatusEnum("status").default("locked").notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
 });
